6abcdfb57b40b21d84b1e362d8590cade49de09b
TeflonDon262 <nate.radtke.89@gmail.com>
2021-11-19 20:44:59 -0500

    Add files via upload

---
+++ custombootflasher.py
@@ -0,0 +1,36 @@
+from sense_hat import SenseHat
+import time
+
+sense = SenseHat()
+
+sense.clear()
+
+#     set_pixel(x, y, r, g, b)
+sense.set_pixel(0, 0, 0, 255, 0)
+
+time.sleep(1)
+sense.clear()
+time.sleep(1)
+
+sense.set_pixel(0, 0, 0, 255, 0)
+
+time.sleep(1)
+sense.clear()
+time.sleep(1)
+
+sense.set_pixel(0, 0, 0, 255, 0)
+
+time.sleep(1)
+sense.clear()
+time.sleep(1)
+
+sense.set_pixel(0, 0, 0, 255, 0)
+
+time.sleep(1)
+sense.clear()
+time.sleep(1)
+
+sense.set_pixel(0, 0, 0, 255, 0)
+
+time.sleep(1)
+sense.clear()
