f39767ba1b6095452ab4ba61bf4c58081fc40b85
TeflonDon262 <nate.radtke.89@gmail.com>
2021-11-20 09:12:23 -0500

    Update custombootflasher.py

--- custombootflasher.py
+++ custombootflasher.py
@@ -8,29 +8,30 @@ sense.clear()
 #     set_pixel(x, y, r, g, b)
 sense.set_pixel(0, 0, 0, 255, 0)
 
-time.sleep(1)
+time.sleep(0.25)
 sense.clear()
-time.sleep(1)
+time.sleep(0.25)
 
 sense.set_pixel(0, 0, 0, 255, 0)
 
-time.sleep(1)
+time.sleep(0.25)
 sense.clear()
-time.sleep(1)
+time.sleep(0.25)
 
 sense.set_pixel(0, 0, 0, 255, 0)
 
-time.sleep(1)
+time.sleep(0.25)
 sense.clear()
-time.sleep(1)
+time.sleep(0.25)
 
 sense.set_pixel(0, 0, 0, 255, 0)
 
-time.sleep(1)
+time.sleep(0.25)
 sense.clear()
-time.sleep(1)
+time.sleep(0.25)
 
 sense.set_pixel(0, 0, 0, 255, 0)
 
-time.sleep(1)
+time.sleep(0.25)
 sense.clear()
+exit
